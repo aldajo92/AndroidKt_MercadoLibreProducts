58c702864bd7ec00c96678d0d283c36434180707
aldajo92 <alejodiscovery20@gmail.com>
2021-04-02 22:54:13 -0500

    fix failed tests

--- core/src/test/java/com/projects/aldajo92/mercadolibreproducts/data/repository/favorites/FavoritesRepositoryImplTest.kt
+++ core/src/test/java/com/projects/aldajo92/mercadolibreproducts/data/repository/favorites/FavoritesRepositoryImplTest.kt
@@ -13,7 +13,6 @@ import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
-import org.mockito.kotlin.mock
 
 @RunWith(MockitoJUnitRunner::class)
 class FavoritesRepositoryImplTest {
